fdf6e25ca6992982c22c942dd28a0ec247e3ba6e
Abdulloh Raxmatullaev <a.raxmatullaev@yagona.io>
2024-10-31 14:00:57 +0500

    remove comments from commented code

--- example/android/app/src/main/kotlin/com/example/myid_wrapper_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/myid_wrapper_example/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myid_wrapper_example
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity(){
-
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
